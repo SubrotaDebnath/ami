6f27538b35eb298bb7f04b0b5a4d30eec1a385c1
Subrota Debnath <subrotadebnath1807@gmail.com>
2023-09-14 17:11:23 +0600

    [FEATURE ADD]
    - Functional Error Handling

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({required this.message});
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({required super.message});
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure({required super.message});
+}
+
+class ConnectionFailure extends Failure {
+  const ConnectionFailure({required super.message});
+}
+
+class DatabaseFailure extends Failure {
+  const DatabaseFailure({required super.message});
+}

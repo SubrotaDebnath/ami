15e07d80e11f73dbca235c4d44adcf2847e8bf0e
Subrota Debnath <subrotadebnath1807@gmail.com>
2023-11-08 16:14:08 +0600

    [CONTACT SCREEN DEMO]

--- lib/single_static_page/presentation/screens/contact_screens/static_desktop_contact_screen.dart
+++ lib/single_static_page/presentation/screens/contact_screens/static_desktop_contact_screen.dart
@@ -10,13 +10,50 @@ class StaticDesktopContactScreen extends StatefulWidget {
 class _StaticDesktopContactScreenState extends State<StaticDesktopContactScreen> {
   @override
   Widget build(BuildContext context) {
-    print('Contact Screen');
     return Container(
-      color: Colors.amber,
-      height: MediaQuery.sizeOf(context).height*.3,
+      color: Colors.blue.shade50,
+      height: 1080,
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-      child: const Center(
-        child: Text('Contact'),
+      child: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            minHeight: 500,
+            maxWidth: 400,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "- LET'S CONNECT",
+                style:
+                    Theme.of(context).textTheme.bodyLarge?.copyWith(height: 4),
+              ),
+              Text(
+                'Get in touch',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineLarge
+                    ?.copyWith(height: 2),
+              ),
+              Text(
+                "I'm currently available to take on new projects, so feel free to send me a message about anything that you want to run past me. You can contact anytime at 24/7",
+                style: Theme.of(context).textTheme.bodyLarge,
+                textAlign: TextAlign.justify,
+              ),
+              // SizedBox(height: 12,),
+              Text(
+                'Dhaka, Bangladesh.',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall
+                    ?.copyWith(height: 2),
+                textAlign: TextAlign.justify,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

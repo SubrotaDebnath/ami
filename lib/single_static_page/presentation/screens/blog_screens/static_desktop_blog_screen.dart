19cb90b84c7a296efe10cfb71402926c721d9bdc
Subrota Debnath <subrotadebnath1807@gmail.com>
2023-11-08 16:39:12 +0600

    [BLOG SCREEN DEMO]

--- lib/single_static_page/presentation/screens/blog_screens/static_desktop_blog_screen.dart
+++ lib/single_static_page/presentation/screens/blog_screens/static_desktop_blog_screen.dart
@@ -11,13 +11,43 @@ class StaticDesktopBlogScreen extends StatefulWidget {
 class _StaticDesktopBlogScreenState extends State<StaticDesktopBlogScreen> {
   @override
   Widget build(BuildContext context) {
-    print('Blog Screen');
     return Container(
-      height: MediaQuery.sizeOf(context).height,
-      color: Colors.red,
+      height: 1080,
+      color: Colors.white,
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-      child: const Center(
-        child: Text('Blog'),
+      child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: 9,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisExtent: 1020 / 3,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+        ),
+        itemBuilder: (context, index) => index == 8
+            ? InkWell(
+                onTap: () {
+                  print('Tapped On See More');
+                },
+                child: Card(
+                  surfaceTintColor: Colors.indigo.shade100,
+                  color: Colors.green.shade50,
+                  shadowColor: Colors.green,
+                  child: Center(
+                      child: Text(
+                    'See more ...',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(color: Colors.indigo),
+                  )),
+                ),
+              )
+            : Card(
+                color: Colors.indigo.shade50,
+                shadowColor: Colors.indigo,
+              ),
       ),
     );
   }
